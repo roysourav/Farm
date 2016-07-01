e8cab909e9113a2775834da60ba25635ec7479f4
Sourav Roy <roysourav1980@gmail.com>
2016-07-02 02:05:07 +0530

    odified cow dead and sell register

--- app/CowSell.php
+++ app/CowSell.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class CowSell extends Model
 {
-    //
+    //gettind the dead cow 
+
+    public function cow()
+    {
+    	return $this->belongsTo('App\Cow');
+    }
 }
